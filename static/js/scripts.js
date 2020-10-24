81690615380889eb1d585f396cf942c496c8d1c7
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-24 17:25:42 +0300

    create the js file for JS codes

---
+++ static/js/scripts.js
@@ -0,0 +1,8 @@
+function openSlideMenu() {
+  document.getElementById('menu').style.width = '250px';
+  document.getElementById('sidenav').style.marginLeft = '250px';
+}
+function closeSlideMenu() {
+  document.getElementById('menu').style.width = '0';
+  document.getElementById('sidenav').style.marginLeft = '0';
+}
