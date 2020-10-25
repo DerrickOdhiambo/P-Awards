ba2ba4d177133202091dd295b0c9015e312d14d9
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-25 09:13:50 +0300

    create the project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=50)
+    project_image = models.ImageField(upload_to='project/')
+    project_description = models.TextField()
+    project_link = models.CharField(max_length=60)
+    project_owner = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
