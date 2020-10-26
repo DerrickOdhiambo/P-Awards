0dc73d34a2b4878b122177555dd495210f2cfcf3
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-26 22:17:33 +0300

    create the user and profile update forms

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,7 @@
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from projects.models import Rating, RATE_CHOICES, Profile
 from django import forms
 
 
@@ -9,3 +11,28 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    full_name = forms.CharField(max_length=50)
+
+    class Meta:
+        model = User
+        fields = ['email', 'full_name', 'username']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_picture', 'user_bio']
+
+
+class RatingForm(forms.ModelForm):
+    review = forms.CharField(widget=forms.Textarea(), required=False)
+    rate = forms.ChoiceField(choices=RATE_CHOICES,
+                             widget=forms.Select(), required=True)
+
+    class Meta:
+        model = Rating
+        fields = ['rate', 'review']
