dd66c3dbeecf18c637ead50b063a4485c60212f1
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-26 22:21:07 +0300

    create routes for login, logout and profile

--- projects/urls.py
+++ projects/urls.py
@@ -2,10 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from .views import ProjectListView, ProjectDetailView, ProjectCreateView, UserProjectListView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='homepage'),
+    path('', ProjectListView.as_view(), name='homepage'),
+    path('user/<str:username>/', UserProjectListView.as_view(), name='user-projects'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+    path('project/new/', ProjectCreateView.as_view(), name='project-create'),
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
