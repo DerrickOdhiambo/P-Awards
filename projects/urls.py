bd7163898a7b0d8abda325c8b709d95befce45cd
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-26 10:47:45 +0300

    create url paths fpr login, logout, registration and profile pages

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='homepage'),
+    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
