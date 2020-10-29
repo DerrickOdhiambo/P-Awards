82a8417e465025fa40bbc6a4968c96e825e6a9f9
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-10-29 08:36:32 +0300

    reduce margin in the homepage

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,3 +6,8 @@ function closeSlideMenu() {
   document.getElementById('menu').style.width = '0';
   document.getElementById('sidenav').style.marginLeft = '0';
 }
+
+// Tooltips Initialization
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
